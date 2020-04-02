6d7569d721537c49f0cfa762bb3db5c517357425
Noah Scala <nwscala@gmail.com>
2020-04-02 14:40:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+
+const num1 = 6.2
+const num2 = 10
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random())
+
+const mod = 9 % 5
+
+const numArray = [1, 20]
+const max = Math.max(...numArray)
